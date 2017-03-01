68a78171a57138c8b08311efb4506a06e6e2e41e
NickyReid <nicky@takealot.com>
2017-03-01 21:59:19 +0200

    fixed path bug

--- fortunelyrics.py
+++ fortunelyrics.py
@@ -11,12 +11,13 @@ import fileinput
 
 def get_song():
     try:
-        topfile = os.listdir('lyricsstash')[0]
-        topfilepath = 'lyricsstash/' + str(topfile)
+        topfile = os.listdir( os.path.dirname(os.path.realpath(__file__)) + '/lyricsstash')[0]
+        topfilepath = os.path.dirname(os.path.realpath(__file__)) + '/lyricsstash/' + str(topfile)
+        print topfilepath
         lyricsfile = open(topfilepath, 'r+')
         output = lyricsfile.read().rstrip('\n')
         lenoutput = len(output)
-
+        #todo: empty files
         print output
         print lenoutput
         os.remove(topfilepath)

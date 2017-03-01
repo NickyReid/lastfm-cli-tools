37f6e814e5cf666929bacaecbd1e24ac498e953d
NickyReid <nicky@takealot.com>
2017-03-01 19:28:18 +0200

    all lyrics in one file

--- fortunelyrics.py
+++ fortunelyrics.py
@@ -4,47 +4,99 @@ import urllib2
 import sys
 import subprocess
 import random
+import time
+import fileinput
 
-api_key = "8257fbe241e266367f27e30b0e866aba"
-last_fm_username = "schiz0rr"
-top_tracks_limit = "50" # The number of results to fetch per
-top_tracks_period = "3month" # overall | 7day | 1month | 3month | 6month | 12month - The time period over which to retrieve top tracks for.
-loved_or_top = random.randint(0,1)
-
-if loved_or_top == 0:
-    api_url = "http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user="+last_fm_username+"&api_key="+api_key+"&format=json"
-else:
-    api_url = "http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user="+last_fm_username+"&api_key="+api_key+"&limit="+top_tracks_limit+"&period="+top_tracks_period+"&format=json"
+begin = time.time()
+lyricsfile = open('lyricsstash/lyrics', 'r+')
+lyrics = lyricsfile.read().splitlines()
+linecount = 0
 
 
-def get_random_song():
-    response = json.loads(urllib2.urlopen(api_url).read())
-    tracks = response.get("lovedtracks").get("track") if loved_or_top == 0 else response.get("toptracks").get("track") 
-    randomtrack = random.choice(tracks)
-    trackname = randomtrack.get('name').replace(')', '').replace('(','').replace("'", "")
-    trackartist = randomtrack.get('artist').get('name')
-    bashCommand = "songtext -t '" + trackname +  "' -a '" + trackartist +"'"    
-    return bashCommand
 
 
-def get_lyrics():   
-    try:
-        song = get_random_song()
-        lyrics = subprocess.check_output(['bash','-c',song])
-        if "Instrumental" in lyrics:
-            return get_lyrics()
+# for line in fileinput.input("test.txt", inplace=True):
+#     print "%d: %s" % (fileinput.filelineno(), line),
+
+def read_new_lyric():
+    first_lyric = False
+    global linecount    
+    for line in lyrics:
+        if '%' in line and not first_lyric:
+            continue
         else:
-            return lyrics
-    except subprocess.CalledProcessError:
-        return get_lyrics()
+            first_lyric = True
 
 
-lyrics = get_lyrics()
-lines =  lyrics.splitlines()
-randomlinenumber = random.randint(0,len(lines))
-for i in range (randomlinenumber+2, randomlinenumber+7):
-    try:
-        if lines[i].strip():
-            print lines[i]
-    except IndexError:
-        pass
+        if '%' not in line:
+            print "%s" % (line)
+            linecount += 1
+            line = lyrics[linecount]
+
+        else:
+            reconstruct(linecount)
+            break    
+
+buffer = ''
+def reconstruct(linecount):
+    lyricsfile.seek(0)
+    lyricsfile.truncate()
+    print linecount            
+    print " === "
+    global buffer
+    for line in range(linecount,len(lyrics)-1):
+        buffer += lyrics[line] + '\n'
+    print buffer    
+    lyricsfile.write(buffer)    
+    # lyricsfile.truncate()
+
+
+
+# l = str(lyrics[15:16])    
+
+
+
+    # for line in lyrics:
+    #     print line
+    # line = lyricsfile.readline().rstrip('\n')
+    # global linecount
+    # while '%' not in line:
+    #     if '$' in line:
+    #         line =lyricsfile.readline()
+    #     else:    
+    #         # global linecount 
+    #         # linecount += 1
+    #         # print linecount
+    #         print "%s" % (line)
+    #         linecount += 1
+
+read_new_lyric()
+end = time.time()
+print end - begin
+
+
+    # for line in fileinput.input('lyricsstash/lyrics', inplace = 1): 
+    #     print line
+    #     if '$' in line: 
+    #         print line.replace("$", "bar")
+    #     else:
+            # pass
+
+    # global linecount
+    # for line in fileinput.input('lyricsstash/lyrics', inplace=True):
+    #     if '$' in line:
+    #         print "%s" % ("asd")
+    #         linecount += 1
+
+
+    # line = lyricsfile.readline().rstrip('\n')
+    # while '%' not in line:
+    #     if '$' in line:
+    #         line =lyricsfile.readline()
+    #     else:    
+    #         # global linecount 
+    #         # linecount += 1
+    #         # print linecount
+    #         print "%s" % (line)
+    #         line = lyricsfile.readline().rstrip('\n')
+

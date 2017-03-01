2906fd97d458cb572a28bc3fe413610a00ce4a7a
NickyReid <nicky@takealot.com>
2017-03-01 20:31:18 +0200

    improved

--- fortunelyrics.py
+++ fortunelyrics.py
@@ -1,5 +1,6 @@
 
 import json
+import os
 import urllib2
 import sys
 import subprocess
@@ -7,96 +8,15 @@ import random
 import time
 import fileinput
 
-begin = time.time()
-lyricsfile = open('lyricsstash/lyrics', 'r+')
-lyrics = lyricsfile.read().splitlines()
-linecount = 0
 
+def get_song():
+    try:
+        topfile = os.listdir('lyricsstash')[0]
+        topfilepath = 'lyricsstash/' + str(topfile)
+        lyricsfile = open(topfilepath, 'r+')
+        print lyricsfile.read().rstrip('\n')
+        os.remove(topfilepath)
+    except IndexError:
+        print "Oh shit, I ran out of songs :("
 
-
-
-# for line in fileinput.input("test.txt", inplace=True):
-#     print "%d: %s" % (fileinput.filelineno(), line),
-
-def read_new_lyric():
-    first_lyric = False
-    global linecount    
-    for line in lyrics:
-        if '%' in line and not first_lyric:
-            continue
-        else:
-            first_lyric = True
-
-
-        if '%' not in line:
-            print "%s" % (line)
-            linecount += 1
-            line = lyrics[linecount]
-
-        else:
-            reconstruct(linecount)
-            break    
-
-buffer = ''
-def reconstruct(linecount):
-    lyricsfile.seek(0)
-    lyricsfile.truncate()
-    print linecount            
-    print " === "
-    global buffer
-    for line in range(linecount,len(lyrics)-1):
-        buffer += lyrics[line] + '\n'
-    print buffer    
-    lyricsfile.write(buffer)    
-    # lyricsfile.truncate()
-
-
-
-# l = str(lyrics[15:16])    
-
-
-
-    # for line in lyrics:
-    #     print line
-    # line = lyricsfile.readline().rstrip('\n')
-    # global linecount
-    # while '%' not in line:
-    #     if '$' in line:
-    #         line =lyricsfile.readline()
-    #     else:    
-    #         # global linecount 
-    #         # linecount += 1
-    #         # print linecount
-    #         print "%s" % (line)
-    #         linecount += 1
-
-read_new_lyric()
-end = time.time()
-print end - begin
-
-
-    # for line in fileinput.input('lyricsstash/lyrics', inplace = 1): 
-    #     print line
-    #     if '$' in line: 
-    #         print line.replace("$", "bar")
-    #     else:
-            # pass
-
-    # global linecount
-    # for line in fileinput.input('lyricsstash/lyrics', inplace=True):
-    #     if '$' in line:
-    #         print "%s" % ("asd")
-    #         linecount += 1
-
-
-    # line = lyricsfile.readline().rstrip('\n')
-    # while '%' not in line:
-    #     if '$' in line:
-    #         line =lyricsfile.readline()
-    #     else:    
-    #         # global linecount 
-    #         # linecount += 1
-    #         # print linecount
-    #         print "%s" % (line)
-    #         line = lyricsfile.readline().rstrip('\n')
-
+get_song()
---
+++ getsongs.py
@@ -0,0 +1,78 @@
+
+import json
+import urllib2
+import sys
+import subprocess
+import random
+import time
+import os
+import fileinput
+from glob import glob
+
+
+def setup():
+    global lyricsfile
+    global api_url
+    global loved_or_top
+    howmanyfiles = len(os.listdir('lyricsstash'))
+    newfilename = get_new_file(howmanyfiles)
+    lyricsfile = open(newfilename, 'ab+')
+    api_key = "8257fbe241e266367f27e30b0e866aba"
+    last_fm_username = "schiz0rr"
+    top_tracks_limit = "100" # The number of results to fetch per
+    top_tracks_period = "6month" # overall | 7day | 1month | 3month | 6month | 12month - The time period over which to retrieve top tracks for.
+    loved_or_top = random.randint(0,1)
+    if loved_or_top == 0:
+        api_url = "http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user="+last_fm_username+"&api_key="+api_key+"&format=json"
+    else:
+        api_url = "http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user="+last_fm_username+"&api_key="+api_key+"&limit="+top_tracks_limit+"&period="+top_tracks_period+"&format=json"
+
+def get_new_file(howmanyfiles): 
+    newfilenumber = howmanyfiles + 1
+    newfilename = 'lyricsstash/' + str(newfilenumber) 
+    try:
+        if os.path.getsize(newfilename) > 0:
+            return get_new_file(howmanyfiles+1)
+    except OSError:
+        pass
+    return newfilename    
+
+def get_random_song():
+    response = json.loads(urllib2.urlopen(api_url).read())
+    tracks = response.get("lovedtracks").get("track") if loved_or_top == 0 else response.get("toptracks").get("track") 
+    randomtrack = random.choice(tracks)
+    trackname = randomtrack.get('name').replace(')', '').replace('(','').replace("'", "")
+    trackartist = randomtrack.get('artist').get('name')
+    bashCommand = "songtext -t '" + trackname +  "' -a '" + trackartist +"'"    
+    return bashCommand
+
+
+def get_lyrics():   
+    try:
+        song = get_random_song()
+        lyrics = subprocess.check_output(['bash','-c',song])
+        if "Instrumental" in lyrics or len(lyrics) < 10:
+            return get_lyrics()
+        else:
+            return lyrics
+    except subprocess.CalledProcessError:
+        return get_lyrics()
+
+def write_file(lyrics):
+    lines =  lyrics.splitlines()
+    randomlinenumber = random.randint(0,len(lines))
+    for i in range (randomlinenumber+2, randomlinenumber+7):
+        try:
+            if lines[i].strip() and not lines[i].isspace() and len(lines[i]) > 1:
+                lyricsfile.write(lines[i] + "\n")
+        except IndexError:
+            pass
+
+try:
+    count = int(sys.argv[1])
+except IndexError:
+    count = 1 
+for i in range(count):           
+    setup()
+    write_file(get_lyrics())
+    lyricsfile.close()

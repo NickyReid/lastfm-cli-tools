e486ae67dd5a904982a92d55331889c509eb3cfc
Nicky Reid <nicky.reid92@gmail.com>
2024-01-07 01:21:40 +0200

    try search without brackets

--- lasthop/spotify.py
+++ lasthop/spotify.py
@@ -1,3 +1,4 @@
+import re
 import spotipy
 import lasthop
 import lastfm_user_data
@@ -49,33 +50,33 @@ class PlaylistMaker:
     def spotify_search(cls, spotify_client, artist, track_name):
         search_result = spotify_client.search(q='track:' + f"{track_name} + {artist}", type='track')
         try:
+            found_item = None
             if search_result.get("tracks"):
                 search_result = search_result["tracks"]
                 if search_result.get("items"):
                     search_result = search_result["items"]
-                else:
-                    return
 
-            found_item = None
+                    found_item = None
 
-            for item in search_result:
-                album = item.get("album")
-                search_artists = album.get("artists")
-                for search_artist in search_artists:
-                    search_artist_name = search_artist.get("name")
-                    # TODO available markets config
-                    if (search_artist_name.lower() in artist.lower() or artist.lower() in search_artist_name.lower()) and "ZA" in album.get("available_markets") and " - live" not in track_name.lower() and "live at " not in track_name.lower():
-                        found_item = item
+                    for item in search_result:
+                        album = item.get("album")
+                        search_artists = album.get("artists")
+                        for search_artist in search_artists:
+                            search_artist_name = search_artist.get("name")
+                            # TODO available markets config
+                            if (search_artist_name.lower() in artist.lower() or artist.lower() in search_artist_name.lower()) and "ZA" in album.get("available_markets") and " - live" not in track_name.lower() and "live at " not in track_name.lower():
+                                found_item = item
+                                break
+                        else:
+                            continue
                         break
-                else:
-                    continue
-                break
 
             if found_item:
                 found_track_uri = found_item.get("uri")
                 return found_track_uri
-            else:
-                print(f"{track_name} by {artist} not found {search_result}")
+            elif ("(" in track_name and ")" in track_name) or ("[" in track_name and "]" in track_name):
+                track_name_without_brackets = re.sub("[\(\[].*?[\)\]]", "", track_name)
+                return cls.spotify_search(spotify_client, artist, track_name_without_brackets)
 
         except Exception as e:
             print(f"Error: {e} {search_result}")
@@ -88,8 +89,9 @@ class PlaylistMaker:
         playlist_name = f"Lasthop {datetime.today().date().strftime('%Y-%m-%d')}"
         playlist = spotify_client.user_playlist_create(user_id, playlist_name, public=False, collaborative=False,
                                                        description="What were you listening to on this day in previous years?")
+        playlist_url = playlist.get("external_urls", {}).get("spotify")
         playlist_id = playlist.get("id")
-        return playlist_id
+        return playlist_id, playlist_url
 
     @classmethod
     def add_track_to_playlist(cls, spotify_client, playlist_id, track_uri, track_name, artist):
@@ -102,13 +104,15 @@ class PlaylistMaker:
             if len(tracks) < 2:
                 continue
             random.shuffle(tracks)
-            selected_track = random.choice(tracks)
+            selected_track = tracks[0]
             found_track_uri = cls.spotify_search(spotify_client, artist, selected_track)
             if found_track_uri:
                 cls.add_track_to_playlist(spotify_client, playlist_id, found_track_uri, selected_track, artist)
             else:
+                current_search = selected_track
                 for retry_track in tracks[1:]:
-                    print(f"Couldn't find '{selected_track}' by {artist}... Searching for '{retry_track}'")
+                    print(f"Couldn't find '{current_search}' by {artist}... Searching for '{retry_track}'")
+                    current_search = retry_track
                     found_track_uri = cls.spotify_search(spotify_client, artist, retry_track)
                     if found_track_uri:
                         cls.add_track_to_playlist(spotify_client, playlist_id, found_track_uri, retry_track, artist)
@@ -125,8 +129,11 @@ class PlaylistMaker:
         if not artist_tracks:
             return
 
-        playlist_id = cls.create_playlist(spotify_client)
-        cls. search_for_tracks(spotify_client, playlist_id, artist_tracks)
+        playlist_id, playlist_url = cls.create_playlist(spotify_client)
+        cls.search_for_tracks(spotify_client, playlist_id, artist_tracks)
+
+        if playlist_url:
+            print(f"Playlist created! {playlist_url}")
 
 
 if __name__ == "__main__":
